f3124f7fbc0be36a041fee11b84841eda6b1c981
wopigjwoigjweroi <agizekel@gmail.com>
2024-10-17 20:52:43 -0700

    commit 5

--- 210-midterm-starter-1.cpp
+++ 210-midterm-starter-1.cpp
@@ -8,30 +8,24 @@ using namespace std;
 
 struct Node {
 
-    string data; 
+    string data;
 
     Node* prev;
 
     Node* next;
 
     Node(string d) : data(d), next(nullptr), prev(nullptr) {}
-}; 
+};
 
 // DoublyLinkedList class is created 
 class DoublyLinkedList {
-private:
+
+public:
 
     Node* head; // Points to the head of the linked list
     Node* tail; // Points to the end of the linked list 
 
-public:
-    DoublyLinkedList() {  // Class constructor initializes head and tail of linked list
-        
-    head = nullptr;
-        
-    tail = nullptr;
-    
-    } 
+    DoublyLinkedList() : head(nullptr), tail(nullptr) {}
 
     void push_back(string n) {
         Node* newNode = new Node(n);
@@ -43,7 +37,7 @@ public:
             tail = newNode;
         }
     }
-    
+
     void push_front(string n) {
         Node* newNode = new Node(n);
         if (!head)
@@ -54,7 +48,7 @@ public:
             head = newNode;
         }
     }
-    
+
     void pop_front() {
 
         if (!head) {
@@ -62,40 +56,40 @@ public:
             return;
         }
 
-        Node * temp = head;
+        Node* temp = head;
 
-        head = head->next; 
+        head = head->next;
 
         if (head) {
-            
+
             head->prev = nullptr;
         }
-            
+
         else
             head = tail = nullptr;
-        
+
         delete temp;
     }
 
     void pop_back() {
-        
+
         if (!tail) {
-            
+
             cout << "Line is empty." << endl;
             return;
         }
         Node* temp = tail;
 
-        tail = tail->prev; 
-        
+        tail = tail->prev;
+
         if (tail) {
-            
+
             tail->next = nullptr;
         }
-            
+
         else
             head = tail = nullptr;
-        
+
         delete temp;
     }
 
@@ -109,38 +103,38 @@ public:
 
     void print() {
         Node* current = head;
-        
+
         while (current) {
-            
+
             cout << current->data << " ";
             current = current->next;
-            
+
         }
         cout << endl;
     }
 
     Node* random_customer() {
 
-        int count = 0; 
+        int count = 0;
 
-        Node* current = head; 
+        Node* current = head;
 
         while (current) {
 
-            count++; 
+            count++;
 
-            current = current->next; 
+            current = current->next;
         }
 
-        int random = rand() % count; 
+        int random = rand() % count;
 
-        current = head; 
+        current = head;
 
         for (int i = 0; i < random; i++) {
 
-            current->next; 
+            current->next;
         }
-        
+
         return current;
     }
 
@@ -148,20 +142,22 @@ public:
     void remove_customer(Node* customer) {
 
         if (customer == head) {
-            
-            pop_front(); 
-            
-        } else if (customer == tail) {
 
-            pop_back(); 
+            pop_front();
 
-        } else {
+        }
+        else if (customer == tail) {
 
-            customer->prev->next = customer->next; 
+            pop_back();
 
-            customer->next->prev = customer->prev; 
+        }
+        else {
 
-            delete customer; 
+            customer->prev->next = customer->next;
+
+            customer->next->prev = customer->prev;
+
+            delete customer;
         }
     }
 };
@@ -171,34 +167,89 @@ vector<string> names(const string& filename) {
 
     vector<string> nameList;
 
-    fstream file("names.txt); 
+    fstream file("names.txt"); 
 
-    string names; 
+        string names;
 
-    while (getline(file, name)) {
+    while (getline(file, names)) {
 
-        nameList.push_back(names); 
+        nameList.push_back(names);
     }
 
-    return nameList; 
+    return nameList;
 }
 
 int main() {
 
-    DoublyLinkedList list; 
+    DoublyLinkedList l;
 
-    list.push_back(1);
-    list.push_back(2); 
-    list.push_back(3);
-    list.push_back(4); 
-    list.push_back(5); 
+    srand(time(0));
 
-    cout << "list";
+    vector<string> nameList = names("names.txt"); 
 
-    list.print(); 
+        cout << "Store opens: \n";
 
-    cout << "Other element"; 
+    for (int i = 0; i < 5; i++) {
 
-    list.every_other_element();     
+        string nameCustomer = nameList[rand() % nameList.size()];
+
+        cout << nameCustomer << " joins the line\n"; 
+
+        l.push_back(nameCustomer); 
+
+    }
+
+    for (int min = 1; min <= 20; min++) {
+
+        cout << "Time step #" << min << ":\n"; 
+
+        if (rand() % 100 < 40 && l.head) {
+
+            cout << l.head->data << "is served\n"; 
+            l.pop_front(); 
+        }
+
+        if (rand() % 100 < 60) {
+
+            string nameCustomer = nameList[rand() % nameList.size()];
+
+            cout << nameCustomer << "joins the line\n"; 
+
+            l.push_back(nameCustomer); 
+        }
+
+        if (rand() % 100 < 20 && l.tail) {
+
+            cout << l.tail->data << " (at the rear) left the line\n"; 
+            l.pop_back(); 
+        }
+
+        if (rand() % 100 < 10 && l.head) {
+
+            Node* randomCustomer = l.random_customer(); 
+
+            if (randomCustomer) {
+
+                cout << randomCustomer->data << " left the line\n"; 
+
+                l.remove_customer(randomCustomer); 
+            }
+
+        }
+
+        if (rand() % 100 < 10) {
+
+            string VIP = nameList[rand() % nameList.size()]; 
+
+            cout << VIP << " (VIP) joins the front of the line\n"; 
+
+            l.push_front(VIP); 
+        }
+
+        cout << "Resulting line: \n";
+
+        l.print(); 
+    }
+
     return 0;
 }
